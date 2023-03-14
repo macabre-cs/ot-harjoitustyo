e417315a1b99cd0f1deb945ca7c9d4db33f41717
Lotta-Linnea Vahter <macabre.cs@gmail.com>
2023-03-14 17:14:50 +0200

    muutoksia

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,28 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    def test_kortin_alkusaldo_ok(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+    
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+    
+    def test_rahan_ottaminen_toimii(self):
+        self.maksukortti.ota_rahaa(200)
+
+        if self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 8.00 euroa"):
+            return True
+        else:
+            return False
+    
+    def test_saldo_ei_muutu_jos_rahaa_ei_ole_tarpeeksi(self):
+        self.maksukortti.ota_rahaa(2000)
+
+        if self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa"):
+            return True
+        
+        else:
+            return False
