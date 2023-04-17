a75854667dbfb05d165b6dfcaaa4426f267e0064
Lotta-Linnea Vahter <macabre.cs@gmail.com>
2023-04-17 09:42:54 +0300

    muutoksia testeihin

--- src/tests/main_view_test.py
+++ src/tests/main_view_test.py
@@ -12,21 +12,9 @@ class TestMainView(unittest.TestCase):
         self.view.destroy()
         self.root.destroy()
     
-    def test_destroy(self):
+    def test_destroy_works(self):
         self.view.destroy()
         self.assertFalse(self.view._frame.winfo_exists())
-
-    def test_love_clicked_works(self):  #testataan toimiiko metodit
-        self.view._love_clicked()
-        
-    
-    def test_feed_clicked_works(self):
-        self.view._feed_clicked()
-        
-    def test_hurt_clicked_works(self):
-        with self.assertRaises(SystemExit):
-            self.view._hurt_clicked()
-            
 
 if __name__=="__main__":
     unittest.main()
